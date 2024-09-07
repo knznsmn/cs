fd68c6d6b8c4b6a4557b6e6bfa6964677abb25b1
Julius C. Cesar <knznsmn@icloud.com>
2024-09-07 18:50:51 +0400

    Add C lessons, exercises.

---
+++ bcfbtp/chapitre_01/01.02_try_again.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("If at first you don't succeed, try again.\n");
+
+  return 0;
+}
---
+++ bcfbtp/chapitre_01/01.03_ides_march.c
@@ -0,0 +1,8 @@
+#include <stdio.h>                        // Preprocessor
+
+int main()                                // main()
+{
+  printf("Beware the Ides of March!\n");  // output
+
+  return 0;                               // returns back
+}                                         // to OS
