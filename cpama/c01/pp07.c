9200d2a4579b9bce23457602cd4fab0f90f55c38
Julius C. Cesar <knznsmn@icloud.com>
2024-09-15 00:27:58 +0400

    Fix pp07.c

--- cpama/c01/pp07.c
+++ cpama/c01/pp07.c
@@ -7,23 +7,23 @@
 int main()
 {
   printf("How much dollars you have in your wallet? ");
-  float total_money;
-  scanf("%f", &total_money);
-  float change;
-  float bills_20 = total_money / 20.0f;
-  change = total_money % 20.0f;
-  float bills_10 = change / 10.0f;
-  change = bills_10 % 10.0f;
-  float bills_05 = change / 5.0f;
-  change = bills_05 % 5.0f;
+  int total_money;
+  scanf("%d", &total_money);
+  int change;
+  int bills_20 = total_money / 20;
+  change = total_money % 20;
+  int bills_10 = change / 10;
+  change = change % 10;
+  int bills_05 = change / 5;
+  change = change % 5;
   //int bills_01 = change / 1;
   //change = (bills_01 % 1) * 10;
 
   printf("\n\n");
-  printf("$20 bills:\t%.0f\n", bills_20);
-  printf("$10 bills:\t%.0f\n", bills_10);
-  printf(" $5 bills:\t%.0f\n", bills_05);
-  printf(" $1 bills:\t%.0f\n", change);
+  printf("$20 bills:\t%d\n", bills_20);
+  printf("$10 bills:\t%d\n", bills_10);
+  printf(" $5 bills:\t%d\n", bills_05);
+  printf(" $1 bills:\t%d\n", change);
   printf("\n\n");
 
   return 0;
