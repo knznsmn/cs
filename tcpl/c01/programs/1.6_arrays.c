8e6a896dee3f510d4a8c06453b2ffd815585b9a9
Julius C. Cesar <knznsmn@icloud.com>
2024-10-05 00:00:16 +0400

    Add array program practice

---
+++ tcpl/c01/programs/1.6_arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int bound = 10;
+  int ndigit[bound];
+
+  for (int i = 0; i < bound; i++)
+  {
+    ndigit[i] = 0;
+    printf("%d\n", ndigit[i]);
+  }
+  return 0;
+}
