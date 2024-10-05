9a317b12d21ae304b62885906e9aaabf674fe4eb
Julius C. Cesar <knznsmn@icloud.com>
2024-10-05 23:59:21 +0400

    Add 1.6 practice problem

---
+++ tcpl/c01/programs/1.6.0_char_counts.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Counts the digits, white spaces, & others.\n");
+  int digits[10];
+  int c, nwhites, nothers;
+
+  nwhites = nothers = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    digits[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF)
+  {
+    if (c >= '0' && c <= '9')
+    {
+      digits[c] = c;
+    }
+    if (c == ' ' || c == '\t' || c == '\n')
+    {
+      nwhites++;
+    }
+    else
+    {
+      nothers++;
+    }
+  }
+  for (int i = 0; i < sizeof(digits)/sizeof(int); i++)
+  {
+    if (i == sizeof(digits)/sizeof(int)))
+      printf("%d.\n", digits[i]);
+    else
+      printf("%d, ", digits[i]);
+  }
+  printf("Number of white spaces are %d.\n", nwhites);
+  printf("Number of other characters are %d.\n", nothers);
+  return 0;
+}
--- tcpl/c01/programs/1.6_arrays.c
+++ tcpl/c01/programs/1.6_arrays.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
 int main() {
-  int bound = 10;
-  int ndigit[bound];
+  int ndigit[10];
+  int c;
 
-  for (int i = 0; i < bound; i++)
+  for (int i = 0; i < sizeof(ndigit) / sizeof(int); i++)
   {
     ndigit[i] = 0;
     printf("%d\n", ndigit[i]);
   }
+  printf("sizeof(ndigit) is %lu.\n", sizeof(ndigit));
+  printf("sizeof(int) is %lu.\n", sizeof(int));
+
+
+  while ((c = getchar()) != EOF)
+  {
+    printf("++ndigit[c - '0' is %d.\n", ++ndigit[c-'0']);
+  }
+
   return 0;
 }
